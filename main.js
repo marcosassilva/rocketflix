759d47f58dd404ff9525478a005443475ed58526
Marcos Silva <maandresouza@gmail.com>
2023-03-20 23:04:05 -0300

    Inserida função de busca por gênero, otimizações

--- main.js
+++ main.js
@@ -4,56 +4,71 @@ import {
   language,
 } from './api.js'
 
+// Armazenar divs manipuladas em variáveis.
+
 let title = document.getElementById('title');
 let description = document.getElementById('description');
 let poster = document.getElementById('poster');
 let releaseDate = ""
 
+// Adicionar Event Listener
 
+document.getElementById("buscarFilme").addEventListener('click', start);
 
-document.getElementById("buscarFilme").addEventListener('click', renderMovie);
+// Função inicial, oculta elemento e renderiza dados.
 
-/* function rodar() {
-  getMovie();
+function start() {
+  title.setAttribute("hidden", "");
+  description.setAttribute("hidden", "");
+  poster.setAttribute("hidden", "");
+  renderMovie();
+} 
 
-  /*   getMovie();
-  if (description.innerText == $0) {
-    getMovie();
-  } */
-
-/*   getMovie()
-  console.log(description.innerHTML) */
-
-/*   do {getMovie()} while (
-    description.innerHTML == ""
-  ) 
-} */
+// Renderizar dados
 
 function renderMovie() {
+  let selectedGenre = document.getElementById('genre').value
+  let validGenre = false
   let id = getRandomID()
+
+  // Consumir API utilizando fetch.
+
   fetch(`${BASE_URL}${id}?${API_KEY}&${language}`)
   .then(response => response.json())
   .then(data => {
-    getMovie(data)
-    while (description.innerHTML == "") {
-      getMovie()
-    }
-    poster.removeAttribute("hidden")
-    title.removeAttribute("hidden")
-    description.removeAttribute("hidden")
+    filterData(data)
 
+    // Se retornar descrição vazia, rodar função novamente.
+
+    if (description.innerHTML == "") {
+      renderMovie()
+    }
+
+    // Rodar função para validar o gênero solicitado.
+
+    validGenre = verifyGenre(data, selectedGenre)
+    console.log(validGenre)
+    console.log(selectedGenre)
+    if (validGenre == false) {
+      renderMovie()
+    } else {
+      title.removeAttribute("hidden");
+      description.removeAttribute("hidden");
+      poster.removeAttribute("hidden");
+    }
   })
   .catch(err => {
     console.log(err)
-    document.querySelector('h1').innerText = "Não encontramos nenhum filme, tente novamente!"
-    document.querySelector('h1').style.textAlign = 'center'
-    poster.setAttribute("hidden", "")
-    title.setAttribute("hidden", "")
-    description.setAttribute("hidden", "")
+
+    // Se retornar erro, rodar função novamente com novo ID.
+
+    renderMovie()
   })
 }
 
-function getMovie(data) {
+// Validação e manipulação de dados obtidos da API
+
+function filterData(data) {
   console.log(data)
   document.querySelector('h1').innerText = "Não sabe o que assistir?"
   releaseDate = data.release_date.slice(0, 4);
@@ -62,50 +77,30 @@ function getMovie(data) {
   poster.src = IMG_URL + data.poster_path
 }
 
+// Função para validar gênero de filme obtido.
+
+function verifyGenre(data, selectedGenre) {
+  let count = 0
+  let validGenre = false
+  for (let i = 0; i < data.genres.length; i++) {
+    if (data.genres[i].name == selectedGenre) {
+      count++
+    }
+  }
+  if (count == 0) {
+    validGenre = false
+  } else {
+    validGenre = true
+  }
+  return validGenre;
+}
+
+// Funções matemáticas
+
 function getRandomID() {
   let id = getRandom(2, 1000)
   return id;
-  
-  
-/*   let success = false;
-
-  while (success === false) {
-      let id = getRandom(2, 700000);
-      fetch(`${BASE_URL}/${id}?${API_KEY}&${language}`)
-      .then(response => response.json())
-      .then(data => {
-        success = data.success
-    } )
-      .catch(err => console.error(err))
-    } 
-    
-    return id */
-    
   }
-
-/*   while (success == false) {
-    let id = getRandom(2, 700000);
-    fetch(`${BASE_URL}/${id}?${API_KEY}&${language}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.success)
-  } )
-    .catch(err => console.error(err))
-  } */
-
-
-/* function rodar() {
-  axios.get(`${BASE_URL}?${API_KEY}`)
-  .then(response => {
-      const data = response.data
-      console.log(data);
-      userName.textContent = data.name
-      userCity.textContent = data.city
-      userID.textContent = data.id
-      userAvatar.src = data.avatar 
-  })
-  .catch(err => console.error(err))
-} */
 
 function getRandom (min, max){
   min = Math.ceil(min);
